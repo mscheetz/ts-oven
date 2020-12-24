9a03fe0b8588d35551cd7b1a229be942470c3dff
Matt Scheetz <mfscheetz@gmail.com>
2020-12-23 19:17:03 -0500

    rest service update

--- src/app/core/rest.service.ts
+++ src/app/core/rest.service.ts
@@ -8,31 +8,33 @@ import { env } from 'yargs';
 @Injectable({providedIn: 'root'})
 export class RestService {
     constructor(private http: HttpClient) {}
+
+    private baseUrl: string = "";
     
     getToken(): Observable<string> {
         const endpoint = environment.LOGIN;
-        const url = `${environment.APIHOST}${endpoint}`;
+        const url = `${this.baseUrl}${endpoint}`;
 
         return this.http.get<string>(url);
     }
 
     bake(dough: IDough): Observable<Blob> {
         const endpoint = environment.BAKE;
-        const url = `${environment.APIHOST}${endpoint}`;
+        const url = `${this.baseUrl}${endpoint}`;
 
         return this.http.post(url, dough, { responseType: 'blob' });
     }
 
     address(symbol: string): Observable<string> {
         const endpoint = `${environment.ADDRESS}/${symbol}`;
-        const url = `${environment.APIHOST}${endpoint}`;
+        const url = `${this.baseUrl}${endpoint}`;
 
         return this.http.get(url, {responseType: 'text'});
     }
 
     getVersions(packageName: string): Observable<string[]> {
         const endpoint = `${environment.VERSIONS}?package=${packageName}`;
-        const url = `${environment.APIHOST}${endpoint}`;
+        const url = `${this.baseUrl}${endpoint}`;
 
         return this.http.get<string[]>(url);
     }
